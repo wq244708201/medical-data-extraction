e099a6b5bbb5a499aa999ff840775e554282c435
wangqian <244708201@qq.com>
2024-11-13 15:43:49 +0800

    fix(build): update version.js with json import assertion

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 // src/config/version.js
-const packageJson = require('../../package.json');
+import pkg from '../../package.json' assert { type: 'json' };
 
-export const APP_VERSION = `v${packageJson.version}`;
+export const APP_VERSION = `v${pkg.version}`;
